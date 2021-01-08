f8843d10ab35dbdb816d00c63c122e584d1e7718
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2021-01-09 00:42:30 +0600

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,10 +1,12 @@
 const firebaseConfig = {
-	apiKey: "AIzaSyBnKanjwWSkpl5Ly1L-v6_oADcBVFfFAn4",
-	authDomain: "click-shop-12.firebaseapp.com",
-	projectId: "click-shop-12",
-	storageBucket: "click-shop-12.appspot.com",
-	messagingSenderId: "60289059985",
-	appId: "1:60289059985:web:835f3bc532a5ca6a294330",
+	apiKey: "API_KEY",
+  	authDomain: "PROJECT_ID.firebaseapp.com",
+  	databaseURL: "https://PROJECT_ID.firebaseio.com",
+  	projectId: "PROJECT_ID",
+  	storageBucket: "PROJECT_ID.appspot.com",
+  	messagingSenderId: "SENDER_ID",
+  	appId: "APP_ID",
+  	measurementId: "G-MEASUREMENT_ID",
 };
 
 export default firebaseConfig;
